636d4f30b1288ae9208ec545c235fb1627dc474c
Dong-Min Shin <shdomi8599@unopnd.com>
2023-08-16 18:45:18 +0900

    main 네트워크 설정 hardhat으로 변경

--- package/hardhat.config.ts
+++ package/hardhat.config.ts
@@ -1,10 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-// 테스트용 계정 키
-const PrivateKey =
-  "d7e6b2426174282c0cde5cde7faef48f334b60350bea795a753e2c25955b0257";
-
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.9",
@@ -15,10 +11,8 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
-    wemix_testnet: {
-      url: "https://api.test.wemix.com",
-      accounts: [`0x${PrivateKey}`],
-      gasPrice: 101000000000,
+    hardhat: {
+      chainId: 31337,
     },
   },
 };
