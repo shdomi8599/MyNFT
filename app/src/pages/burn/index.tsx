e778396c8120c8b1b67591f0c36b7e4b6fe2e210
Dong-Min Shin <shdomi8599@unopnd.com>
2023-08-17 16:07:14 +0900

    main 소각 기능 구현

--- app/src/pages/burn/index.tsx
+++ app/src/pages/burn/index.tsx
@@ -1,3 +1,62 @@
+import { abi, contractAddress } from "@/const";
+import { useMetaMask } from "@/hook";
+import { Box, Button } from "@mui/material";
+import { useEffect } from "react";
+import { useContractWrite } from "wagmi";
+import { waitForTransaction } from "wagmi/actions";
+
 export default function Burn() {
-  return <div>burn</div>;
+  const { isLogin } = useMetaMask();
+
+  const {
+    data: burn,
+    write,
+    isLoading: isBurnLoading,
+  } = useContractWrite({
+    address: contractAddress,
+    abi,
+    functionName: "burn",
+    args: [BigInt(1)],
+    onError: (e) => {
+      console.log(e);
+    },
+  });
+
+  const onBurn = () => {
+    write?.();
+  };
+
+  useEffect(() => {
+    const getReceipt = async () => {
+      const { hash } = burn! || {};
+      const receipt = await waitForTransaction({ hash });
+      if (receipt) {
+        console.log(receipt);
+      }
+    };
+
+    if (burn?.hash) {
+      getReceipt();
+    }
+  }, [burn]);
+
+  return (
+    <Box
+      sx={{
+        height: "90vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Button
+        sx={{ width: "15%", height: "10%", fontSize: "1rem" }}
+        variant="contained"
+        disabled={isBurnLoading || !isLogin}
+        onClick={onBurn}
+      >
+        Burn
+      </Button>
+    </Box>
+  );
 }
